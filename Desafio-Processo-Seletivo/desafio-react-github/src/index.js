4687d83f2431cfcbabab83fde541a01151740aa3
Vitor Manoel Melo <vitormmbrito12@gmail.com>
2021-05-25 23:46:32 -0300

    Desafio Adicionado

---
+++ Desafio-Processo-Seletivo/desafio-react-github/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom' 
+
+import './index.css';
+
+import App from './pages/home/App';
+import reportWebVitals from './reportWebVitals';
+import Repositorio from './pages/repositorios/repositorio'
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/repositorios" component={Repositorio} />
+        <Redirect to="/" component={App}/>
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ Desafio-Processo-Seletivo/desafio-react-github/src/pages/repositorios/repositorio.js
@@ -0,0 +1,117 @@
+import {Component} from 'react'
+
+import './repositorio.css'
+
+class Repositorio extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            listaRepositorios : [],
+            username: ''
+        }
+    };
+
+    listarRepositorios = () =>{
+
+        fetch(`https://api.github.com/users/${this.state.username}/repos`)
+
+        .then(resposta => resposta.json())
+
+        .then(dados => this.setState({ listaRepositorios : dados}))
+        
+        .catch(erro => console.log(erro))
+    }
+
+    componentDidMount(){
+        // this.listarRepositorios()
+    }
+
+    render(){
+        return (
+            <div>
+                <main>
+                    <section> Sessão de listagem dos repositórios
+                        <h2>Lista de Repositórios</h2>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Id Repositório</th>
+                                    <th>Nome</th>
+                                    <th>Descricão</th>
+                                    <th>Data de Criação</th>
+                                    <th>Tamanho</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {
+                                    this.state.listaRepositorios.map( (repositorio) =>{
+                                        return(
+                                            <tr>
+                                                <td>{repositorio.id}</td>
+                                                <td>{repositorio.name}</td>
+                                                <td>{repositorio.description}</td>
+                                                <td>{repositorio.created_at}</td>
+                                                <td>{repositorio.size / 1000} mb</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+
+                    </section>
+
+                    {/* <section>
+                        <h2>Buscar Repositório</h2>
+                        <form onSubmit={this.cadastrar}>
+                            <div>
+                                <input
+                                    type="text"
+                                    value={this.state.username}
+                                    onChange={this.atualizarEstado}
+                                    placeholder="Username"
+                                />
+
+                                <button type="submit">Buscar</button>
+                            </div>
+                        </form>
+                    </section> */}
+
+                    {/* <section className="listagem">
+                        <div className="content">
+                            {
+                                this.state.listaRepositorios.map( (repositorio) => {
+                                    return(
+                                      <div className="repositorio">
+                                          <div className="dados">
+                                            <p><strong>Id:</strong> {repositorio.id}</p>
+                                          </div>
+                                          <div className="dados">
+                                            <p><strong>Nome:</strong> {repositorio.name}</p>
+                                          </div>
+                                          <div className="dados">
+                                            <p><strong>Descrição:</strong> {repositorio.description}</p>
+                                          </div>
+                                          <div className="dados">
+                                            <p><strong>Data de Criação:</strong> {repositorio.created_at}</p>
+                                          </div>
+                                          <div className="dados">
+                                            <p><strong>Tamanho:</strong> {repositorio.size / 1000} mb</p>
+                                          </div>
+                                      </div>  
+                                    )
+                                }).reverse()
+                            }
+                        </div>
+                    </section> */}
+
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Repositorio;
